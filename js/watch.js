f92a313acc82ab11c6362223c63d5beaf18a9078
beloyosry <105988333+beloyosry@users.noreply.github.com>
2023-01-23 19:21:13 +0200

    Add files via upload

---
+++ js/watch.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $("#myBtn").on("click", function () {
+    read();
+  });
+  $(".reply").click(function () {
+    $(this).parents("div.row").next("div.card").toggle(300);
+  });
+  $(".dropdown-toggle").click(function () {
+    $(".dropdown-menu").toggleClass("show");
+  });
+});
+function read() {
+  let dots = document.getElementById("dots");
+  let moreText = document.getElementById("more");
+  let btnText = document.getElementById("myBtn");
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "عرض المزيد";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "عرض عناصر أقل";
+    moreText.style.display = "inline";
+  }
+}
